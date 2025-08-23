cabead384a902aeee78de5aa52594b9484b867ab
shallow <sxnchit452@gmail.com>
2025-08-22 17:58:21 -0700

    created programs that add and multiply two numbers

---
+++ Exercises/divide.py
@@ -0,0 +1,11 @@
+def divide(a, b):
+    return a / b
+
+def main():
+    x = 5
+    y = 7
+    z = divide(x, y)
+    print(f"{x} / {y} = {z}")
+
+main()
+
---
+++ Exercises/multiply.py
@@ -0,0 +1,11 @@
+def multiply(a, b):
+    return a * b
+
+def main():
+    x = 5
+    y = 7
+    z = multiply(x, y)
+    print(f"{x} * {y} = {z}")
+
+main()
+
